61145f47a2ec086eabb84c1f40dc853ba24573cd
Shihab <shihabunimap@gmail.com>
2023-05-01 11:53:34 +0200

    Delete 0x0A-configuration_management directory

--- 0x0A-configuration_management/0-create_a_file.pp
+++
@@ -1,9 +0,0 @@
-# This manifesto creates a file called school with the text I love Puppet
-file { '/tmp/school':
-ensure  => present,
-path    => '/tmp/school',
-mode    => '0744',
-owner   => 'www-data',
-group   => 'www-data',
-content => 'I love Puppet',
-}
--- 0x0A-configuration_management/2-execute_a_command.pp
+++
@@ -1,5 +0,0 @@
-# This will ensure that a command is executed
-exec {'killmenow':
-command => 'pkill killmenow',
-path    => '/usr/bin:/usr/sbin:/bin'
-}
